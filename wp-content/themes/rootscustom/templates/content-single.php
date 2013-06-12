ed849f28df8638dfb36ae8dd15c6bda890622efb
browngd <browngd@gmail.com>
2013-06-11 21:32:08 -0400

    Changed post title size
    Changed post title from h1 to h2

--- wp-content/themes/rootscustom/templates/content-single.php
+++ wp-content/themes/rootscustom/templates/content-single.php
@@ -1,7 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
